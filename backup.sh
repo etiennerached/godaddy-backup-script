1ee06e1e482c735b8ae519df2f4116379f35654e
Etienne Rached <etiennerached@users.noreply.github.com>
2013-10-27 14:27:03 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 2.0
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is
+# Etienne Rached
+# http://www.tech-and-dev.com/2013/10/backup-godaddy-files-and-databases.html
+# Portions created by the Initial Developer are Copyright (C) 2013
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#
+# ***** END LICENSE BLOCK *****
+
 ###################### Configuration ######################
 
 #Store the backups in the following directory
